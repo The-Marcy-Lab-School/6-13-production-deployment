843c62b85b2ca656d99b7af7e3300751d4534149
Jonathan <JohnAnBer@gmail.com>
2026-04-27 13:01:18 -0400

    l

--- server/db/pool.js
+++ server/db/pool.js
@@ -1,23 +1,24 @@
-// ✍️ TODO 1: Import dotenv and run config()
-
-const { Pool } = require("pg");
+// 1. Import dotenv and run config(). Must be called before any process.env references
 require("dotenv").config();
-console.log(process.env);
+const { Pool } = require("pg");
 
-// ✍️ TODO 2: Replace hard-coded values with process.env
-
-const config = {
+// 2. Replace hard-coded values with `process.env`
+const devConfig = {
   host: process.env.PGHOST,
   port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
-  // user: 'username',
-  // password: 'password',
 };
 
-// ✍️ TODO 3: Create a separate config for production environments that use a connectionString
+// 3. Create this separate config for production environments where we'll have a connection string
+const prodConfig = {
+  connectionString: process.env.PG_CONNECTION_STRING,
+};
 
-// ✍️ TODO 4: If PG_CONNECTION_STRING is available, use the prodConfig, otherwise use devConfig
-
-const pool = new Pool(config);
+// 4. Use PG_CONNECTION_STRING if available, otherwise use individual credentials.
+const pool = new Pool(
+  process.env.PG_CONNECTION_STRING ? prodConfig : devConfig
+);
 
 module.exports = pool;
